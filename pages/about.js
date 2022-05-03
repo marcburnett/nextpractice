d3620431c5ee92edcdb0cd7b8238a5fdd7ec48a9
Marc Burnett <marc_burnett@yahoo.com>
2022-05-03 18:33:42 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+
+const About = () => {
+    return ( 
+        <div>
+        
+            <h1>About Page</h1>
+
+            <p>This is my first attempt to building a site using Next.js</p>
+
+               
+
+        </div>
+     );
+}
+ 
+export default About;
